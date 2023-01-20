7851f2ed2d2abe252a0356c8b9bba636d4983807
sabwally <95961990+sabwally@users.noreply.github.com>
2023-01-20 10:26:25 +0300

    It's magic

--- lab3_3/main.cpp
+++ lab3_3/main.cpp
@@ -13,18 +13,21 @@ int main() {
 	g.add_node(&v4);
 	Edge u1(g.get_node(v1.get_id()), g.get_node(v2.get_id()), 10);
 	Edge u2(g.get_node(v1.get_id()), g.get_node(v3.get_id()), 100);
-	Edge u3(g.get_node(v2.get_id()), g.get_node(v4.get_id()), 9);
-	Edge u4(g.get_node(v3.get_id()), g.get_node(v4.get_id()), 40);
+	Edge u3(g.get_node(v2.get_id()), g.get_node(v4.get_id()), 40);
+	Edge u4(g.get_node(v3.get_id()), g.get_node(v4.get_id()), 9);
+	Edge u5(g.get_node(v1.get_id()), g.get_node(v4.get_id()), 64);
 	g.add_edge(&u1);
 	g.add_edge(&u2);
 	g.add_edge(&u3);
 	g.add_edge(&u4);
+	g.add_edge(&u5);
 	cout << "sanlc" << endl;
 	g.get_node(v1.get_id())->get_edges()->push_back(u1);
 	g.get_node(v1.get_id())->get_edges()->push_back(u2);
 	g.get_node(v2.get_id())->get_edges()->push_back(u3);
-	//g.get_node(v3.get_id())->get_edges()->push_back(u4);
-	
+	g.get_node(v3.get_id())->get_edges()->push_back(u4);
+	g.get_node(v1.get_id())->get_edges()->push_back(u5);
+
 	vector<int> path = Dijkstra(g, v1.get_id(), v2.get_id());
 	//cout << "fevuwycb " << g.get_node(v1.get_id())->get_edges()->size() << endl;
 	cout << "vbibn " << path.size() << endl;
@@ -32,7 +35,12 @@ int main() {
 		cout << path[i] << endl;
 	}
 	cout << "cbkdn" << endl;
-
+	vector<int> path2 = Dijkstra(g, v1.get_id(), v4.get_id());
+	cout << "rbek " << path2.size() << endl;
+	for (int i = 0; i < path2.size(); ++i) {
+		cout << path2[i] << endl;
+	}
+	cout << "dsvsdc" << endl;
 	//for (int i = 0; i < g.get_list_nodes()->size(); ++i) {
 	//	cout << (*g.get_list_nodes())[i].get_id() << endl;
 	//}
